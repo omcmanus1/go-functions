19e1fb782ff62c57eb6b8251e621656a864006fa
omcmanus1 <omcmanus1@gmail.com>
2024-05-20 21:57:37 +0100

    make tests generic + update for slices

--- utils.go
+++ utils.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type Primitives interface {
+	bool | string | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr
+}
+
 // compare output with expected value
 // return error if not equal
-func CheckGotWant(t testing.TB, got, want interface{}) {
+func CheckGotWantPrimitive[P Primitives](t testing.TB, got, want P) {
 	t.Helper()
 	if got != want {
+		t.Errorf(`got %v want %v`, got, want)
+	}
+}
+
+func CheckGotWantSlice[P Primitives](t testing.TB, got, want []P) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf(`got %v want %v`, got, want)
 	}
 }

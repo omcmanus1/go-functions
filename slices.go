d0913f70f396bf860c97ab45408a80f8795e87a3
omcmanus1 <omcmanus1@gmail.com>
2024-05-29 22:24:40 +0100

    update slices

--- slices.go
+++ slices.go
@@ -1,9 +1,18 @@
 package main
 
-// Go passes by values for int and string
-// but arrays, slices and maps are mutated unless you copy
-func CopySlice[T any](arr []T) []T {
-	arrCopy := make([]T, len(arr))
-	copy(arrCopy, arr)
-	return arrCopy
+func MutateSlice[T Primitives](slc []T) []T {
+	switch slc := any(slc).(type) {
+	case []string:
+		slc[2] = "hello"
+	case []int:
+		slc[2] = 42
+	case []bool:
+		slc[2] = true
+	}
+	return slc
+}
+
+func CopySlice[T Primitives](slc []T) []T {
+	slcCopy := make([]T, len(slc))
+	return slcCopy
 }
--- slices_test.go
+++ slices_test.go
@@ -5,22 +5,10 @@ import (
 	"testing"
 )
 
-func mutateSlice[P Primitives](arr []P) []P {
-	switch arr := any(arr).(type) {
-	case []string:
-		arr[2] = "hello"
-	case []int:
-		arr[2] = 42
-	case []bool:
-		arr[2] = true
-	}
-	return arr
-}
-
-func TestCopySlice(t *testing.T) {
+func TestMutateSlice(t *testing.T) {
 	t.Run("mutated slice should not equal original value", func(t *testing.T) {
 		inp := []string{"a", "b", "c", "d"}
-		mutateSlice(inp)
+		MutateSlice(inp)
 		got := inp
 		want := []string{"a", "b", "c", "d"}
 		if reflect.DeepEqual(got, want) {
@@ -29,15 +17,18 @@ func TestCopySlice(t *testing.T) {
 	})
 	t.Run("uncopied slice should be directly mutated by function", func(t *testing.T) {
 		inp := []string{"a", "b", "c", "d"}
-		mutateSlice(inp)
+		MutateSlice(inp)
 		got := inp
 		want := []string{"a", "b", "hello", "d"}
 		CheckGotWantPrimitive(t, got[2], want[2])
 	})
+}
+
+func TestCopySlice(t *testing.T) {
 	t.Run("copied slice of strings should not be mutated by function", func(t *testing.T) {
 		inp := []string{"a", "b", "c", "d"}
 		inpCopy := CopySlice(inp)
-		mutateSlice(inpCopy)
+		MutateSlice(inpCopy)
 		got := inp
 		want := []string{"a", "b", "c", "d"}
 		CheckGotWantSlice(t, got, want)
@@ -45,7 +36,7 @@ func TestCopySlice(t *testing.T) {
 	t.Run("copied slice of ints should not be mutated by function", func(t *testing.T) {
 		inp := []int{0, 0, 0, 0, 0, 0}
 		inpCopy := CopySlice(inp)
-		mutateSlice(inpCopy)
+		MutateSlice(inpCopy)
 		got := inp
 		want := []int{0, 0, 0, 0, 0, 0}
 		CheckGotWantSlice(t, got, want)
